fae57b15088f8110b8ea02ca9b0d50a73fb0b0b9
Mateusz Pieczulis <mateuszpieczulis123@gmail.com>
2023-11-09 09:27:34 +0100

    add more ants in tests

--- tests/test_antsLogic.py
+++ tests/test_antsLogic.py
@@ -9,12 +9,13 @@ def test_fps():
 
 def test_logic():
     engine = Engine(testRun=True)
-    engine.fps = 10
+    engine.fps = 100
     engine.grid.addFood((1, 11))
     engine.grid.addAntNest((780, 780))
-    engine.grid.addAnts((780, 798))
+    for _ in range(100):
+        engine.grid.addAnts((780, 798))
     
     avgFps = engine.run()
 
 
-    assert avgFps > 10
+    assert avgFps > 100
