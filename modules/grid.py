db86548f3c5701faa431b026d229c8dd8ca1e70e
Mateusz Pieczulis <mateuszpieczulis123@gmail.com>
2023-11-16 09:19:09 +0100

    remove max opty in update trail

--- modules/grid.py
+++ modules/grid.py
@@ -85,9 +85,8 @@ class Moves(Position):
 class TrailExpiration():
     def updateTrailExpiration(self, blocks: list[int]) -> None:
         for y, col in enumerate(blocks):
-            if max(col) != 0:
-                for x, block in enumerate(col):
-                    self.expirate(x, y, block)
+            for x, block in enumerate(col):
+                self.expirate(x, y, block)
 
     def expirate(self, x: int, y: int, value: int) -> None:
         if 0 < value <= 100:
